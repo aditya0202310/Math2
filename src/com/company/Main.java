f1f4bbffa267d738c3de48689f46717052908cc3
aditya0202310 <66565008+aditya0202310@users.noreply.github.com>
2020-10-03 00:37:43 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+public class Main {
+    public static int timeLeft(){
+         
+
+    }
+}
+
+        /*System.out.println(dayOfWeek(0, 1));
+        System.out.println(dayOfWeek(0, 14));
+        System.out.println(dayOfWeek(6, 22));
+        System.out.println(dayOfWeek(5, 4));
+        System.out.println(dayOfWeek(1, 24));
+        System.out.println(dayOfWeek(2, 1));
+    }
+}*/
+
+    /*public static int digitSwapper(int input){
+        int y = input/ 10 % 10;
+        int x = (input / 100) * 100;
+        int z = (input % 10) * 10;
+        return x + y + z;
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int input = 123;
+        int output = digitSwapper(input);
+        System.out.println(input + " " + output);
+
+        input = 6;
+        output = digitSwapper(input);
+        System.out.println(input + " " + output);
+
+        input = 381;
+        output = digitSwapper(input);
+        System.out.println(input + " " + output);
+
+
+    }
+}*/
